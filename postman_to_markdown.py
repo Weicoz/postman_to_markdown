52142524687c6b3ca86f947bb940891b430b96c8
lijiawei <weicoz@outlook.com>
2021-11-24 16:36:02 +0800

    新增module，ignoreQuery，ignoreBody，

--- postman_to_markdown.py
+++ postman_to_markdown.py
@@ -8,7 +8,7 @@ import json
 from os.path import isfile, join
 
 config = {}
-
+template = 'temp2.md'
 
 def list_all_files(file_path):
     return [f for f in os.listdir(file_path) if isfile(join(file_path, f))]
@@ -97,31 +97,34 @@ def pluck_body(detail):
     body = ""
     if "body" in detail['request']:
         mode = detail['request']['body']['mode']
-        for v in detail['request']['body'][mode]:
-            description = ''
-            types = v['type']
-            value = ''
-            if 'disabled' in v.keys():
-                continue
-            if 'value' in v.keys():
-                value = v['value']
-                if v['value'] is None:
-                    value = ''
-            elif 'src' in v.keys():
-                value = v['src']
-            if "description" in v:
-                description = v['description']
-                if "|" in v['description']:
-                    desc_arr = v['description'].split("|")
-                    types = desc_arr[0]
-                    description = desc_arr[1]
-
-            body += "| " + v['key'] + " | " + types + " | " + description + " | " + value + " |\n"
+        if mode == 'raw':
+            body += detail['request']['body'][mode] + '\n';
+        else:
+            for v in detail['request']['body'][mode]:
+                description = ''
+                types = v['type']
+                value = ''
+                if 'disabled' in v.keys():
+                    continue
+                if 'ignoreBody' in config and v['key'] in config['ignoreBody']:
+                    continue
+                if 'value' in v.keys():
+                    value = v['value']
+                    if v['value'] is None:
+                        value = ''
+                elif 'src' in v.keys():
+                    value = v['src']
+                if "description" in v:
+                    if "|" in v['description']:
+                        description = v['description'].replace('|', ' | ')
+                    else:
+                        description = types + "|" + v['description'] + "| 否"
+                body += "| " + v['key'] + " | " + description + " | " + value + " |\n"
         if not body:
             return ""
     else:
         return ""
-    return "**参数：**\n\n|参数名|类型|说明|参考参数|\n|:---- |:-----|:----- |-----   |\n%s" % body
+    return "**POST参数：**\n\n|参数名|类型|必填|说明|参考参数|\n|:---- |:-----|:----- |:----- |----- |\n%s" % body
 
 
 def pluck_query(detail):
@@ -133,6 +136,8 @@ def pluck_query(detail):
             value = ''
             if 'disabled' in v.keys():
                 continue
+            if 'ignoreQuery' in config and v['key'] in config['ignoreQuery']:
+                continue
             if 'value' in v.keys():
                 value = v['value']
                 if v['value'] is None:
@@ -140,18 +145,16 @@ def pluck_query(detail):
             elif 'src' in v.keys():
                 value = v['src']
             if "description" in v:
-                description = v['description']
                 if "|" in v['description']:
-                    desc_arr = v['description'].split("|")
-                    types = desc_arr[0]
-                    description = desc_arr[1]
-
-            query += "| " + v['key'] + " | " + types + " | " + description + " | " + value + " |\n"
+                    description = v['description'].replace('|', ' | ')
+                else:
+                    description = types + "|" + v['description'] + "| 否"
+            query+= "| " + v['key'] + " | " + description + " | " + value + " |\n"
         if not query:
             return ""
     else:
         return ""
-    return "**参数：**\n\n|参数名|类型|说明|例子|\n|:---- |:-----|:----- |-----   |\n%s" % query
+    return "**GET参数：**\n\n|参数名|类型|必填|说明|参考参数|\n|:---- |:-----|:----- |:----- |----- |\n%s" % query
 
 
 def pluck_description(detail):
@@ -168,6 +171,7 @@ def pluck_result(detail):
             for v in detail['response']:
                 vv = json.loads(v['body'])
                 result += "```\n" + json.dumps(vv, indent=4, ensure_ascii=False) + "\n```\n"
+            result = "**返回示例**\n\n" + result
             return result
     return ""
 
@@ -257,18 +261,48 @@ def set_def_parame(name, key, use_def_parame=True):
     return param + " | %s | %s |\n" % (param_type, param_str)
 
 
+"""
+save_markdown:
+url 读取配置文件内的参数
+filename 文件类型
+description 接口说明
+method 请求类型
+header 头部内容
+query GET接口参数
+body POST接口参数
+result 返回示例
+explan 返回参数说明
+"""
+
+
 def save_markdown(url='', filename='makedown.md', description='', method='', header='', query='', body='', result='',
                   explan=''):
     url = config['host'] + re.sub("{{host}}", '', url)
     url = re.sub("/[0-9]+", '/{{id}}', url)
-    string = load_template() % (description, url, method, header, query, body, result, explan)
+    if "module" in config:
+        if 'description' in config['module']:
+            description = description if config['module']['description'] else ''
+        if 'url' in config['module']:
+            url = url if config['module']['url'] else ''
+        if 'method' in config['module']:
+            method = method if config['module']['method'] else ''
+        if 'header' in config['module']:
+            header = header if config['module']['header'] else ''
+        if 'query' in config['module']:
+            query = query if config['module']['query'] else ''
+        if 'body' in config['module']:
+            body = body if config['module']['body'] else ''
+        if 'result' in config['module']:
+            result = result if config['module']['result'] else ''
+        if 'explan' in config['module']:
+            explan = explan if config['module']['explan'] else ''
+    string = load_template(template) % (description, url, method, header, query, body, result, explan)
     save_file(filename, string)
     return True
 
 
 def load_template(filename='temp1.md'):
-    template = open('./template/' + filename, 'r', encoding='UTF-8').read()
-    return template
+    return open('./template/' + filename, 'r', encoding='UTF-8').read()
 
 
 run()
